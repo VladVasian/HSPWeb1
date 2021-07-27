fc1627c2ef15ac3dd9a3b45383e3f59a416b4422
vladv <vlad.vasian@diginesis.com>
2021-07-27 16:58:09 +0300

    Add project files.

---
+++ WebApplication1/Client/Models/BankAccount.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Client.Models
+{
+    public class BankAccount
+    {
+        public int Id { get; set; }
+
+        public string Iban { get; set; }
+    }
+}
---
+++ WebApplication1/Client/Models/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Client.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public int Mgnr { get; set; }
+        public int MBNO { get; set; }
+        public string Name { get; set; }
+        public string Title { get; set; }
+        public string Suffix { get; set; }
+        public string Company { get; set; }
+        public bool ActivityStatus { get; set; }
+        public string Status { get; set; }
+        public string PayerStatus { get; set; }
+        public double Payment { get; set; }
+        public string City { get; set; }
+        public int BankAccountID { get; set; }
+    }
+}
